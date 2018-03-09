3c8adc8dbae6f46776361011c780a2beb7aeaee4
daniel_gurbiel <daniel.gurbiel@gmail.com>
2018-03-09 21:43:26 +0100

    1.0

---
+++ Higher-Lower/src/higher/lower/GameHigherLower.java
@@ -0,0 +1,19 @@
+package higher.lower;
+
+/**
+ *
+ * @author Daniel Gurbiel
+ */
+public class GameHigherLower {
+    private int range;
+    
+    public int getRange(){
+        return range;
+    }
+    
+    public void setRange(int range){
+        this.range = range;
+    }
+    
+    
+}
--- Higher-Lower/src/higher/lower/HigherLower.java
+++ Higher-Lower/src/higher/lower/HigherLower.java
@@ -1,12 +1,19 @@
 package higher.lower;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Daniel Gurbiel
  */
 public class HigherLower {
+    private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
+        GameHigherLower game = new GameHigherLower();
+        System.out.println("Hello! This is game: higher / lower. Let's start! Enjoy!");
+        System.out.print("Set range: ");
+        game.setRange(input.nextInt());
         
     }
     
