fae1a091bc5f868a18ee7bb66adce3cabb928089
daniel_gurbiel <daniel.gurbiel@gmail.com>
2018-03-11 10:51:05 +0100

    1.0

--- Higher-Lower/src/higher/lower/GameHigherLower.java
+++ Higher-Lower/src/higher/lower/GameHigherLower.java
@@ -1,11 +1,27 @@
 package higher.lower;
 
+import java.util.InputMismatchException;
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author Daniel Gurbiel
  */
+
 public class GameHigherLower {
     private int range;
+    private static int number_of_attempts;
+    private static Scanner input;
+    private int random_value;
+    private Random generator;
+
+    public GameHigherLower() {
+        number_of_attempts = 0;
+        range = 0;
+        input = new Scanner(System.in);
+        generator = new Random();
+    }
     
     public int getRange(){
         return range;
@@ -15,5 +31,39 @@ public class GameHigherLower {
         this.range = range;
     }
     
+    public int getNumberOfAttempts(){
+        return number_of_attempts;
+    }
+    
+    public void increaseNumberOfAttempts(){
+        number_of_attempts++;
+    }
+    
+    public void play(){
+        generateRandomValue();
+        int result;
+        
+        try{
+            
+            do{
+                System.out.print("Insert value: ");
+                result = input.nextInt();
+                increaseNumberOfAttempts();
+                if(result > random_value) System.out.println("Too large...");
+                else if(result < random_value) System.out.println("To small...");
+                
+            } while(result != random_value);
+            
+        } catch (InputMismatchException e) {
+            System.out.println("Input by user was not a number.");
+            e.printStackTrace();
+        }  
+        
+        System.out.print("You won! The number of your attempts is: " + number_of_attempts + ".");
+    }
+    
+    public void generateRandomValue(){
+        random_value = generator.nextInt(range)+1;
+    }
     
 }
--- Higher-Lower/src/higher/lower/HigherLower.java
+++ Higher-Lower/src/higher/lower/HigherLower.java
@@ -1,20 +1,30 @@
 package higher.lower;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /**
  *
  * @author Daniel Gurbiel
  */
+
 public class HigherLower {
     private static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
         GameHigherLower game = new GameHigherLower();
         System.out.println("Hello! This is game: higher / lower. Let's start! Enjoy!");
-        System.out.print("Set range: ");
-        game.setRange(input.nextInt());
-        
-    }
-    
+        try{
+            int range;
+            do{
+                System.out.print("Set range: ");
+                range = input.nextInt();
+            } while(range < 1);
+            game.setRange(range);
+            game.play();
+        } catch (InputMismatchException e) {
+            System.out.println("Input by user was not a number.");
+            e.printStackTrace();
+        }   
+    } 
 }
